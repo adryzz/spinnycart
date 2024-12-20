c59b4ddcdc216bbfeabfaf9b8a7415a6ef6413b4
Lena <lena@nihil.gay>
2024-12-20 09:44:39 +0100

    add https

--- spinnycart/src/config.rs
+++ spinnycart/src/config.rs
@@ -25,57 +25,95 @@ pub async fn load_config_file() -> anyhow::Result<Conf> {
 #[derive(Config, Debug, Clone)]
 pub struct Conf {
     #[config(nested)]
-    network: NetworkConf,
+    pub network: NetworkConf,
     #[config(nested)]
-    limits: LimitsConf,
+    pub limits: LimitsConf,
+    #[config(nested)]
+    pub auth: AuthConf,
 }
 
 #[derive(Config, Debug, Clone)]
 pub struct NetworkConf {
     #[config(env = "HTTP_PORT", default = 8080)]
-    http_port: u16,
+    pub http_port: u16,
+
+    #[config(env = "HTTPS_PORT")]
+    pub https_port: Option<u16>,
 
     #[config(env = "ADDRESS", default = "127.0.0.1")]
-    address: IpAddr,
+    pub address: IpAddr,
 
-    #[config(env = "BEHIND_PROXY", default = "false")]
-    behind_proxy: bool,
+    #[config(env = "BEHIND_PROXY", default = false)]
+    pub behind_proxy: bool,
 
     #[config(env = "BASE_URL", default = "http://127.0.0.1:8080")]
-    base_url: String,
+    pub base_url: String,
+
+    #[config(env = "TLS_CERT_PATH")]
+    pub tls_cert_path: Option<PathBuf>,
+
+    #[config(env = "TLS_KEY_PATH")]
+    pub tls_key_path: Option<PathBuf>,
 }
 
 #[derive(Config, Debug, Clone)]
 pub struct LimitsConf {
     /// Max message size (bytes)
-    /// 
+    ///
     /// Default: 4K
     #[config(default = 4096)]
     message_size_limit: u32,
 
     /// Minimum delay for Scheduled Delivery (seconds)
-    /// 
+    ///
     /// Default: 10s
     #[config(default = 10)]
     message_delay_min_limit: u32,
 
     /// Maximum delay for Scheduled Delivery (seconds)
-    /// 
+    ///
     /// Default: 3 days
     #[config(default = 259200)]
     message_delay_max_limit: u32,
 
     /// Maximum number of topics before the server starts rejecting them
-    /// 
+    ///
     /// Default: 15000
     #[config(default = 15000)]
     global_topic_limit: u32,
 
     /// Maximum number of subscriptions (open connections) per visitor
-    /// 
+    ///
     /// Default: 30
     #[config(default = 30)]
     visitor_subscription_limit: u32,
-
     // TODO: add request limits (when not running behind a proxy)
+}
+
+#[derive(Config, Debug, Clone)]
+pub struct AuthConf {}
+
+
+impl NetworkConf {
+    pub fn https(&self) -> bool {
+        match (self.https_port, &self.tls_cert_path, &self.tls_key_path) {
+            (None, None, None) => return false,
+            (Some(_), Some(_), Some(_)) => return true,
+            _ => {
+                if self.https_port.is_none() {
+                    tracing::error!("HTTPS is enabled, but no HTTPS port was configured.");
+                }
+                if self.tls_cert_path.is_none() {
+                    tracing::error!("HTTPS is enabled, but no TLS certificate was configured.");
+                }
+
+                if self.tls_key_path.is_none() {
+                    tracing::error!("HTTPS is enabled, but no TLS certificate key was configured.");
+                }
+
+                tracing::warn!("Running in HTTP-only mode.");
+                return false
+            }
+        }
+    }
 }
--- spinnycart/src/main.rs
+++ spinnycart/src/main.rs
@@ -1,3 +1,9 @@
+use std::{net::SocketAddr, sync::Arc};
+
+use axum::{Router, routing::get};
+use axum_server::tls_rustls::RustlsConfig;
+use tower_http::trace::TraceLayer;
+
 mod config;
 mod log;
 
@@ -5,9 +11,58 @@ mod log;
 async fn main() -> anyhow::Result<()> {
     log::tracing_init()?;
 
-    let config = config::load_config_file().await?;
+    let config = Arc::new(config::load_config_file().await?);
 
     tracing::info!("spinnycart UnifiedPush server started.");
 
+    let state = Arc::new(WebState {
+        config: config.clone(),
+    });
+
+    let app = Router::new()
+        .route("/", get(|| async { "test" }))
+        .layer(TraceLayer::new_for_http())
+        .with_state(state);
+
+    
+    let service = app.into_make_service_with_connect_info::<SocketAddr>();
+
+    if config.network.https() {
+        let rustls = RustlsConfig::from_pem_file(
+            config.network.tls_cert_path.as_deref().unwrap(),
+            config.network.tls_cert_path.as_deref().unwrap(),
+        ).await?;
+
+        let addr = SocketAddr::new(
+            config.network.address,
+            config.network.http_port,
+        );
+
+        tracing::info!("Listening HTTPS on {}...", addr);
+
+        axum_server::bind_rustls(addr, rustls).serve(service).await?;
+    } else {
+        let listener = tokio::net::TcpListener::bind(SocketAddr::new(
+            config.network.address,
+            config.network.http_port,
+        ))
+        .await?;
+        tracing::info!("Listening HTTP on {}...", listener.local_addr()?);
+    
+        axum::serve(
+            listener,
+            service,
+        )
+        .await?;
+    }
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct WebState {
+    pub config: Arc<config::Conf>,
+}
+
+async fn redirect_http_to_https(config: Arc<config::Conf>) {
+    // https://github.com/tokio-rs/axum/blob/main/examples/tls-rustls/src/main.rs
 }
